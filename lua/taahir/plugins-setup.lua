305d0aecaf8d200d4e61c5b1fd991a8581f56644
ta4h1r <taahirbhaiyat01@gmail.com>
2024-10-09 16:17:40 +0200

    Add back telescope to plugin import

--- lua/taahir/plugins-setup.lua
+++ lua/taahir/plugins-setup.lua
@@ -57,7 +57,14 @@ return packer.startup(function(use)
 
 	-- fuzzy finding w/ telescope
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+	use({
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x", -- fuzzy finder
+		requires = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
 	-- autocompletion
 	use("hrsh7th/nvim-cmp") -- completion plugin
