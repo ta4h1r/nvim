363cc4f8d6c9e154c28e81dba2a59d5848d2001b
ta4h1r <taahirbhaiyat01@gmail.com>
2024-10-13 01:49:46 +0200

    Add telescope resume keymap

--- lua/taahir/core/keymaps.lua
+++ lua/taahir/core/keymaps.lua
@@ -82,6 +82,7 @@ keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files wit
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
 -- telescope git commands (not on youtube nvim video)
