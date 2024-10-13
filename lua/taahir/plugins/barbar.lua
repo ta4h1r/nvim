9b34a262f00fa488693c871c4cccf7eb1c750252
ta4h1r <taahirbhaiyat01@gmail.com>
2024-10-13 02:05:57 +0200

    Add close all but current buffer barbar keymap

--- lua/taahir/plugins/barbar.lua
+++ lua/taahir/plugins/barbar.lua
@@ -22,6 +22,7 @@ map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
 map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
 -- Close buffer
 map("n", "<A-w>", "<Cmd>BufferClose<CR>", opts)
+map("n", "<A-q>", "<Cmd>BufferCloseAllButCurrent<CR>", opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
