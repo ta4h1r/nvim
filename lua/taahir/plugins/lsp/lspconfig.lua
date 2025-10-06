2509f6704b1325a9d2d05db3e9e4d80a69dc0029
ta4h1r <taahirbhaiyat01@gmail.com>
2025-10-06 12:53:00 +0200

    Cleanup unused import

--- lua/taahir/plugins/lsp/lspconfig.lua
+++ lua/taahir/plugins/lsp/lspconfig.lua
@@ -1,9 +1,3 @@
--- import lspconfig plugin safely
--- local lspconfig_status, lspconfig = pcall(require, "lspconfig")
--- if not lspconfig_status then
--- 	return
--- end
-
 -- import cmp-nvim-lsp plugin safely
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
